ed6720f35f0a4bee50d5e0109a7ee0093c336bae
Marco <marcosilvafonseca@gmail.com>
2018-09-26 10:54:43 -0500

    added another chapter 1 exercise

--- learncppTutorial/chapter1/doublenumber.cpp
+++
@@ -1,6 +0,0 @@
-#include "doublenumber.h"
-
-//this function multiplies an integer by 2
-int doubleNumber(int n) {
-    return 2 * n;
-}
--- learncppTutorial/chapter1/doublenumber.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DOUBLENUMBER_H
-#define _DOUBLENUMBER_H
-
-
-
-    int doubleNumber(int n);
-
-
-#endif
